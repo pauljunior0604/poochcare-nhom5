be2d34e3a27c33c97c81aa9907d977c0c33b4b3d
pauljuniordev <139752925+pauljunior0604@users.noreply.github.com>
2023-08-30 20:09:33 +0700

    helloo
    update file Hien

---
+++ index.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("moon_icoin");
+
+
+let changeIcon = function (icon) {
+  icon.classList.toggle("bi-sun");
+}
+function darkTheme (){
+
+ var element = document.body;
+  element.classList.toggle("dark-mode");
+ 
+}
